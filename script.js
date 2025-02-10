0913651854ece3eda6dcae80cd2e7249a66b3afe
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2025-02-10 18:16:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function generateImage() {
 
     for (let y = 0; y < canvas.height; y++) {
         for (let x = 0; x < canvas.width; x++) {
-            let color = Math.floor(Math.random() * 255);
-            ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
             ctx.fillRect(x, y, 1, 1);
         }
     }
